50a46ef33b2f4b55f990697525786cb676c11e7f
Oh <osm723@gmail.com>
2025-07-14 10:50:54 +0900

    project init

---
+++ src/test/java/com/oh/pharmacy/Haein/HaeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oh.pharmacy.Haein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
